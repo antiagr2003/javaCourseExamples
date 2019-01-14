fb34a5436a56098b29014d2ba036823e409aa3b6
David Contreras <davidcb@comillas.edu>
2019-01-14 13:36:56 +0100

    Práctica 0 y retos

---
+++ 00a.retos/reto00/Main.java
@@ -0,0 +1,11 @@
+/*
+Reto #0:
+Muestra en la consola el texto "Hola Mundo!"
+*/
+class Main 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hola Mundo!");
+    }
+}
