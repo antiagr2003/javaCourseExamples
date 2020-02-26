b120c901b5b7f3f3764803bc6fd30d14a9d567e5
David Contreras <davidcb@comillas.edu>
2020-02-26 10:37:04 +0100

    Ejemplo de Relación de Objetos

---
+++ 06c.relacionObjetos/Jugador.java
@@ -0,0 +1,63 @@
+/** @author David Contreras */
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Jugador
+{
+    private String nombre;
+    private int dorsal;
+    private Collection estadisticas;
+
+    public Jugador(String nombre, int dorsal)
+    {
+        this.nombre = nombre;
+        this.dorsal = dorsal;
+        estadisticas = new ArrayList();
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }    
+    
+    public Collection getEstadisticas()
+    {
+        return estadisticas;
+    }
+
+    public void addEstadisticas(Estadistica e)
+    {
+        estadisticas.add(e);
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if(o instanceof Jugador)
+        {
+            Jugador jugador = (Jugador) o;
+            return nombre.equals(jugador.getNombre());
+        }
+        else
+            return false;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return dorsal%5;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append(nombre)
+            .append(" - #")
+            .append(dorsal)
+            .append(" - Estadisticas: ")
+            .append(estadisticas);
+        return sb.toString();        
+    }    
+}
