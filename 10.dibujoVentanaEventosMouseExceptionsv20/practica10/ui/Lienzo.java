e40fc8c17a3107ac8fb0440c130fb2ad7678a2a0
David Contreras <davidcb@comillas.edu>
2020-04-03 12:52:13 +0200

    Práctica 10 con mejor diseño y funcionalidad

---
+++ 10.dibujoVentanaEventosMouseExceptionsv20/practica10/ui/Lienzo.java
@@ -0,0 +1,102 @@
+package practica10.ui;
+
+import practica10.dominio.Figura;
+import practica10.dominio.Circulo;
+import practica10.dominio.Cuadrado;
+import practica10.dominio.Dibujo;
+import practica10.dominio.FiguraNoValidaException;
+
+import practica10.util.RGBConversionException;
+import practica10.util.Util;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Cursor;
+import java.awt.Color;
+import java.awt.Point;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+
+import javax.swing.JOptionPane;
+
+import java.util.Collection;
+
+
+public class Lienzo extends javax.swing.JPanel
+{
+	private JVentanaDibujo ventanaDibujo;
+	private Dibujo dibujo;
+
+	private Point puntoInicial;
+
+	public Lienzo(JVentanaDibujo ventanaDibujo)
+	{
+		this.ventanaDibujo = ventanaDibujo;
+		this.dibujo = ventanaDibujo.getDibujo();
+
+		this.setBackground(Color.WHITE);
+		this.setPreferredSize(new java.awt.Dimension(800, 600));
+		this.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
+		this.addMouseListener(new MouseAdapter()
+			{
+				public void mouseClicked(MouseEvent me)
+				{
+					Lienzo.this.gestionarEventoMouse(me);
+				}
+			});		
+	}
+
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g); 
+		Collection<Figura> figuras = dibujo.getTodasFiguras();
+		for(Figura figura:figuras)
+			figura.pintar(g);
+	}
+
+	private void gestionarEventoMouse(MouseEvent me)
+	{
+		try
+		{
+			Color color = ventanaDibujo.validarColores();
+			if(puntoInicial == null)
+			{
+				puntoInicial = new Point(me.getX(), me.getY());
+				Lienzo.this.setCursor(new Cursor(Cursor.SE_RESIZE_CURSOR));						
+			}
+			else
+			{
+				Point puntoFinal = new Point(me.getX(), me.getY());
+				int dimension = puntoFinal.x - puntoInicial.x;
+				if(dimension<0)
+				{
+					puntoInicial = puntoFinal;
+					dimension = Math.abs(dimension);
+				}
+
+				Figura figura = null;
+				if(ventanaDibujo.getTipoFiguraAPintar()==JVentanaDibujo.TipoFiguras.CUADRADO)
+					figura = new Circulo(puntoInicial.x, puntoInicial.y, true, color, dimension);	
+				else
+					figura = new Cuadrado(puntoInicial.x, puntoInicial.y, true, color, dimension);	
+
+				dibujo.addFigura("Grupo Global", figura);
+				Lienzo.this.repaint();
+				puntoInicial = null;
+				Lienzo.this.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
+				ventanaDibujo.asignarColoresRandom();
+			}
+		}
+		catch(NumberFormatException exc)
+		{
+			JOptionPane.showMessageDialog(Lienzo.this, "El color debe ser un número");
+		}
+		catch(RGBConversionException exc)
+		{
+			JOptionPane.showMessageDialog(Lienzo.this, exc.toString());	
+		}
+	}
+
+}
