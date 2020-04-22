3393e9308d63584195eb3e18ede91839eb4db07a
David Contreras <davidcb@comillas.edu>
2020-04-22 11:47:00 +0200

    Ejemplo de Imagen de fondo y ventana maximizada

---
+++ 10c10.swing/JPanelImagen.java
@@ -0,0 +1,41 @@
+import java.awt.*;
+import java.awt.image.*;
+import javax.swing.*;
+
+public class JPanelImagen extends JPanel
+{
+    private BufferedImage imagen;
+
+    public JPanelImagen()
+    {
+        try
+        {
+            imagen = javax.imageio.ImageIO.read(new java.io.File("java.jpg"));
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+        this.setLayout(new FlowLayout());
+        this.add(new JButton("Botón 1"));
+        this.add(new JButton("Botón 2"));
+        this.add(new JButton("Botón 3"));        
+    }
+
+    public int getAncho()
+    {
+        return imagen.getWidth();
+    }
+
+    public int getAlto()
+    {
+        return imagen.getHeight();
+    }    
+
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        g.drawImage(imagen, 0, 0, this);
+    }
+}
