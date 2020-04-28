7367b233aea8a52d93b5233caae0b3bbdda13853
David Contreras <davidcb@comillas.edu>
2020-04-28 11:56:31 +0200

    Ejemplo de librerías de 3rd. Jsoup & JFreeChart

--- 10c3.3rd-webscrapping/run.bat
+++ 10c3.3rd-webscrapping/run.bat
@@ -1,2 +1,2 @@
-javac -cp .;jcommon-1.0.23.jar;jfreechart-1.0.19.jar;jsoup-1.13.1.jar covid19\JVentana.java 
-java -cp .;jcommon-1.0.23.jar;jfreechart-1.0.19.jar;jsoup-1.13.1.jar covid19.JVentana 
+javac -cp .;lib\jcommon-1.0.23.jar;lib\jfreechart-1.0.19.jar;lib\jsoup-1.13.1.jar covid19\JVentana.java 
+java -cp .;lib\jcommon-1.0.23.jar;lib\jfreechart-1.0.19.jar;lib\jsoup-1.13.1.jar covid19.JVentana 
