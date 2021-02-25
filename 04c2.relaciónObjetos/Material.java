1d1e8003c7173cc7e9084e8f982653d501f31f81
David Contreras <davidcb@comillas.edu>
2021-02-25 17:41:10 +0100

    Ejemplo de relación de objetos

---
+++ 04c2.relaciónObjetos/Material.java
@@ -0,0 +1,22 @@
+public class Material
+{
+	private String tipo;
+	private int cantidad;
+
+	public Material(String tipo, int cantidad)
+	{
+		this.tipo = tipo;
+		this.cantidad = cantidad;
+	}
+
+	public void picar(int material)
+	{
+		cantidad -= material;
+	}
+
+	@Override
+	public String toString()
+	{
+		return tipo + "(" + cantidad + ")";
+	}
+}
