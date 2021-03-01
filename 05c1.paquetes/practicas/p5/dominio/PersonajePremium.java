035f1516ee6d6caccf9720f16b667ab573cc5957
David Contreras <davidcb@comillas.edu>
2021-03-01 10:29:31 +0100

    Ejemplo de Paquetes

---
+++ 05c1.paquetes/practicas/p5/dominio/PersonajePremium.java
@@ -0,0 +1,39 @@
+package practicas.p5.dominio;
+
+public class PersonajePremium extends Personaje
+{
+	private String skins;
+
+	public PersonajePremium(String skins)
+	{
+		this.skins = skins;
+	}
+
+	public PersonajePremium(int vida, String skins)
+	{
+		super(vida);
+		this.skins = skins;
+	}
+
+	public PersonajePremium(int vida, Material material, String skins)
+	{
+		super(vida, material);
+		this.skins = skins;
+	}
+
+	public String getSkins()
+	{
+		return skins;
+	}
+
+	public void setSkins(String skins)
+	{
+		this.skins = skins;
+	}	
+
+	@Override
+	public String toString()
+	{
+		return super.toString() + " Skins: " + skins;
+	}	
+}
