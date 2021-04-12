9be26ddc406176f525eb63670af40895b3d44f85
David Contreras <davidcb@comillas.edu>
2021-04-12 08:16:26 +0200

    Prueba algoritmos

---
+++ 10prueba.algoritmos/Util.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Util 
+{
+    public static HashMap<Integer,Integer> encontrarPareja(int[] numeros, int sum) 
+    {
+    /* Código a desarrollar
+    *
+    *
+    *
+    * */
+        return null;
+    }
+
+    public static HashMap<Integer, Integer> escribirParejasSimetricas(HashMap<Integer,Integer> hm) 
+    {
+    /* Código a desarrollar
+    *
+    *
+    *
+    * */
+        return null;
+    }
+
+
+    public static boolean tieneTodosNumConsecutivos(int[] b) 
+    {
+    /* Código a desarrollar
+    *
+    *
+    *
+    * */
+        return true;
+    }
+}
