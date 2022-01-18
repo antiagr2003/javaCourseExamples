35b27f37a0ae00c76b5db8f7139ce779b9421d2f
David Contreras <davidcb@comillas.edu>
2022-01-18 10:01:19 +0100

    Ejemplo de Objetos

---
+++ grupoA/01c.objetos/AppPersonaje.java
@@ -0,0 +1,24 @@
+public class AppPersonaje
+{
+	public static void main(String args[]) 
+	{
+		Personaje p1; //Definción del objeto
+		p1 = new Personaje(); //Creación del objeto. Llamando al constructor de la clase.
+
+		Personaje p2 = new Personaje();
+
+		p1.setVida(12);
+
+		p1.disparar(p2);
+
+
+		String s;
+		s = "Hola";
+		String s2 = "Hola";
+
+
+
+	}
+
+
+}
