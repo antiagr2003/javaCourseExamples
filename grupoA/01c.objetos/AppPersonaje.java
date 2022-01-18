d4394c516c2b6a1b22d50e282bebfc471ee814e8
David Contreras <davidcb@comillas.edu>
2022-01-18 11:03:00 +0100

    Ejemplo de definición de objeto

--- grupoA/01c.objetos/AppPersonaje.java
+++ grupoA/01c.objetos/AppPersonaje.java
@@ -5,20 +5,18 @@ public class AppPersonaje
 		Personaje p1; //Definción del objeto
 		p1 = new Personaje(); //Creación del objeto. Llamando al constructor de la clase.
 
-		Personaje p2 = new Personaje();
+		Personaje p2 = new Personaje(); //Definición y creación del objeto
 
-		p1.setVida(12);
-
+		p1.setVida(12); //Mensajes 
 		p1.disparar(p2);
 
-
+		/* 
+		Ejemplo de la mismas operaciones, 
+		pero con otro tipo de objeto:String 
+		*/
 		String s;
 		s = "Hola";
 		String s2 = "Hola";
-
-
-
+		s2.toUpperCase();
 	}
-
-
 }
