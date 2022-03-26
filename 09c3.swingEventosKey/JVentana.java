d53138536bedd24506c62ea0ba7586e2f1c1ee94
David Contreras <davidcb@comillas.edu>
2022-03-26 11:23:12 +0100

    Ejemplo de Eventos KeyListener

---
+++ 09c3.swingEventosKey/JVentana.java
@@ -0,0 +1,137 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.KeyAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+
+import java.awt.Font;
+import java.awt.Color;
+
+/*
+Q1: ¿Qué diferencia existe entre keyPressed y keyTyped a la hora de tomar el valor de las teclas?
+Q2: ¿Qué son los atributos VK_xxxx definidos en KeyEvent?
+Q3: ¿Qué hace el método requestFocus() de Component?
+
+*/
+
+
+public class JVentana extends JFrame
+{
+    private JButton btnAceptar;
+    private JButton btnCancelar;
+
+    public static void main(String[] args)
+    {
+        new JVentana();
+    }
+
+	public JVentana()
+	{
+        super("Personas");
+
+        JPanel pnlNorte = new JPanel(new FlowLayout());
+        JPanel pnlCentro = new JPanel(new GridLayout(3, 2));
+        JPanel pnlSur = new JPanel(new GridLayout(1, 2));
+
+        JLabel lblTitulo = new JLabel("Gestión de Personas");
+        lblTitulo.setFont(new Font("Arial", Font.BOLD, 16));
+        lblTitulo.setForeground(new Color(112, 146, 190));
+        pnlNorte.add(lblTitulo);
+
+        btnAceptar = new JButton("Aceptar");
+        btnCancelar = new JButton("Cancelar");
+
+        JLabel lblNombre = new JLabel("Nombre: ");
+        JLabel lblEdad = new JLabel("Edad: ");
+
+        JTextField txtNombre = new JTextField(12);
+        JTextField txtEdad = new JTextField(12); //Caracteres
+
+        pnlCentro.add(lblNombre);
+        pnlCentro.add(txtNombre);
+        pnlCentro.add(lblEdad);
+        pnlCentro.add(txtEdad);
+        pnlCentro.add(btnAceptar);
+        pnlCentro.add(btnCancelar);
+
+        pnlSur.add(new JLabel("v1.0"));
+        JLabel lblBy = new JLabel("by DCB");
+        lblBy.setHorizontalAlignment(JLabel.RIGHT); //Alineación a la derecha
+        pnlSur.add(lblBy);
+
+        btnAceptar.addActionListener(new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    System.out.println("Botón pulsado Aceptar");
+                }
+            });
+
+        btnCancelar.addActionListener(new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    System.out.println("Botón pulsado Cancelar");
+                }
+            });
+
+        //this.addWindowListener(this);
+        //this as JFrame.recibe eventos de Ventana que gestionará(this as WindowListener);
+
+        this.addWindowListener(new WindowAdapter()
+            {
+                @Override
+                public void windowClosing(WindowEvent e)   
+                {
+                    System.out.println("Desea realmente cerrar la aplicación");
+                    JVentana.this.dispose();
+                    System.exit(0);
+                }
+            });
+
+        txtNombre.addKeyListener(new KeyAdapter()
+        	{
+        		@Override
+                public void keyPressed(KeyEvent e)   
+                {
+                	int code = e.getKeyCode();
+                	if(code == KeyEvent.VK_ENTER)
+                		txtEdad.requestFocus();
+
+                    System.out.println("Pressed:" + e.getKeyCode() + "-" + e.getKeyChar());
+                }
+
+        		@Override
+                public void keyTyped(KeyEvent e)   
+                {
+                    System.out.println("Typed: " + e.getKeyCode() + "-" + e.getKeyChar());
+                }                
+        	});
+
+        this.setLayout(new BorderLayout()); //JFrame
+        this.add(pnlNorte, BorderLayout.NORTH);
+        this.add(pnlCentro, BorderLayout.CENTER);
+        this.add(pnlSur, BorderLayout.SOUTH);
+        
+        this.pack();
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE); //Para que al pulsar en la X finalice la aplicación
+        //Este método siempre debe ser el último mensaje
+        this.setVisible(true);
+	}
+
+}
