957826a529b26e2111a73db014caeab38a02f3cf
David Contreras <davidcb@comillas.edu>
2022-04-22 16:30:31 +0200

    Ejemplo de plataforma de juego con Scrolling y efecto Parallax

---
+++ 99.examples/99.plataformaJuego/EspacioScrolling/src/ui/interfaces/Controlable.java
@@ -0,0 +1,9 @@
+package ui.interfaces;
+
+import java.awt.event.KeyEvent;
+
+public interface Controlable
+{
+    public void keyReleased(KeyEvent e);
+    public void keyPressed(KeyEvent e);
+}
---
+++ 99.examples/99.plataformaJuego/EspacioScrolling/src/ui/objetos/Sprite.java
@@ -0,0 +1,91 @@
+package ui.objetos;
+
+import app.Juego;
+
+import java.awt.*;
+import javax.swing.ImageIcon;
+
+public class Sprite
+{
+    protected int x;
+    protected int y;
+
+    protected float xLogica;
+
+    protected int ancho;
+    protected int alto;
+
+    protected boolean visible;
+    protected Image imagen;
+
+    protected float velocidad;
+
+    public Sprite(int x, int y, float velocidad, String ficheroImagen)
+    {
+        this.x = x;
+        this.xLogica = x;
+        this.y = y;
+        this.velocidad = velocidad;
+        visible = true;
+        this.loadImage(ficheroImagen);
+    }
+
+    public static Sprite parse(String[] elementos)
+    {
+        int x = Integer.parseInt(elementos[1]);
+        int y = Integer.parseInt(elementos[2]);
+        float velocidad = Float.parseFloat(elementos[3]);
+        String ficheroImagen = elementos[4];
+        Sprite sprite = new Sprite(x, y, -velocidad, ficheroImagen);
+        return sprite;
+    }
+
+    private void loadImage(String ficheroImagen)
+    {
+        imagen = new ImageIcon(Juego.DIR_RESOURCES + ficheroImagen).getImage();
+        ancho = imagen.getWidth(null);
+        alto = imagen.getHeight(null);
+    }    
+
+    public Image getImagen()
+    {
+        return imagen;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public boolean isVisible()
+    {
+        return visible;
+    }
+
+    public void setVisible(boolean visible)
+    {
+        this.visible = visible;
+    }
+
+    public void mover()
+    {
+        xLogica += velocidad;
+        x = (int) xLogica;
+
+        if (x > Juego.ANCHO)
+            visible = false;
+        else if (x < 0 - this.ancho)
+            xLogica = x = Juego.ANCHO;
+    }
+
+    public void pintar(Graphics2D g)
+    {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(imagen, x, y, null);
+    }
+}
