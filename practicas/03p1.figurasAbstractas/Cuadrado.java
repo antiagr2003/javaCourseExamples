6e4c8d4e9679dc576239459288cc1787a7845254
David Contreras <davidcb@comillas.edu>
2023-02-10 12:58:17 +0100

    Práctica 3p1 Figura getInfo - Solución

---
+++ practicas/03p1.figurasAbstractas/Cuadrado.java
@@ -0,0 +1,38 @@
+import java.awt.Color;
+
+public class Cuadrado extends Figura
+{ 
+	int lado;
+
+	/** 
+		Inicializa todos los atributos del objeto 
+		@param lado Tamaño del cuadrado en pixels
+	*/
+	Cuadrado(int x, int y, boolean relleno, Color color, int lado)
+	{
+		super(x, y, relleno, color);
+		this.setLado(lado);
+	}
+
+	void setLado(int lado)
+	{
+		if(x > 1 && x < 600)
+			this.lado = lado;
+		else
+			this.lado = 2;
+	}
+
+	int getLado()
+	{
+		return lado;
+	}
+
+	@Override
+	String getInfo()
+	{
+		String s = "Información del cuadrado: ";
+		s += super.getInfo();
+		s += " Lado: "+ lado;
+		return s;
+	} 
+}
