0519d8f4f1d6bbf1634b75c123bc35755435d984
David Contreras <davidcb@comillas.edu>
2023-03-15 20:06:15 +0100

    Código de los vídeos #1 y #2 en GitHub: ejemplo de ArrayList

---
+++ practicas/07.dibujoGruposFuncional/practica07/ui/Lienzo.java
@@ -0,0 +1,74 @@
+package practica07.ui;
+
+import practica07.dominio.Figura;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+
+import java.util.Iterator;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
+public class Lienzo extends Canvas
+{
+	private java.util.HashMap<String, Collection<Figura>> figuras = new java.util.HashMap<String, Collection<Figura>>();
+
+	public void pintar(String grupo, Figura figura)
+	{
+		Collection<Figura> figurasGrupo = figuras.get(grupo);
+		if(figurasGrupo==null)
+		{
+			figurasGrupo = new java.util.ArrayList<Figura>();
+			figurasGrupo.add(figura);
+			figuras.put(grupo, figurasGrupo);
+		}
+		else
+			figurasGrupo.add(figura);	
+	}
+
+	/*
+	interface Stream
+	void forEach(Consumer<? super T> action) --> acción
+	*/
+	public void setVisibleGrupo(String grupo, boolean visible)
+	{
+		Collection<Figura> figurasGrupo = figuras.getOrDefault(grupo, new java.util.ArrayList<Figura>());
+		figurasGrupo.stream()
+					.forEach(figura -> figura.setVisible(visible));
+	}	
+
+	public void setVisibleTodasFiguras(boolean visible)
+	{
+		figuras.values()
+			.stream()
+			.flatMap(Collection::stream)
+			.forEach(figura -> figura.setVisible(visible));
+	}
+
+	/*
+
+	filter(Predicate<? super T> predicate) --> condición
+	flatMap(Function<? super T,? extends Stream<? extends R>> mapper)
+	*/
+	@Override
+	public void paint(Graphics g)
+	{
+		//Con referencias a métodos 
+		figuras.values()
+			   .stream()
+			   .flatMap(Collection::stream)
+   			   .filter(Figura::isVisible)
+			   .forEach(figura -> figura.pintar(g));
+			   
+		/*
+		Con Lambdas:
+		figuras.values()
+			   .stream()
+			   .flatMap(valores -> valores.stream())
+   			   .filter(figura -> figura.isVisible())
+			   .forEach(figura -> figura.pintar(g));
+		*/
+	}
+}
