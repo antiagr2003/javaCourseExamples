3a0c267aed0ff5ded52677465be539a2c673d16e
David Contreras <davidcb@comillas.edu>
2023-03-15 20:08:21 +0100

    Reto #1. Rendimiento de las colecciones tipo List

---
+++ retos/07reto.List/AppRetoList.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AppRetoList
+{
+    public static void main(String[] args)
+    {
+        System.out.println("****ArrayList");
+        AppRetoList.calculaTiempos(new ArrayList<Persona>());
+        System.out.println("****LinkedList");
+        AppRetoList.calculaTiempos(new LinkedList<Persona>());
+    } 
+
+    private static void calculaTiempos(List<Persona> estructura)
+    {
+        int numeroObjetos = 1000*1000*10;
+
+        long tiempoIni = System.currentTimeMillis();
+        for(int i=0;i<numeroObjetos;i++)
+            estructura.add(new Persona(String.valueOf(i)));  
+
+        System.out.println("Creacion: " + estructura.getClass().getName() + " -> " + (System.currentTimeMillis()-tiempoIni) + "ms");
+
+        tiempoIni = System.currentTimeMillis();
+        System.out.print("Busqueda: " + estructura.contains(new Persona("No lo encuentro")) + " -> ");
+        System.out.println(estructura.getClass().getName() + ": " + (System.currentTimeMillis()-tiempoIni) + "ms");
+
+        tiempoIni = System.currentTimeMillis();
+        System.out.print("Acceso por indice a la posicion SIZE/2: " + estructura.get((int)(numeroObjetos/2)) + " -> ");
+        System.out.println(estructura.getClass().getName() + ": " + (System.currentTimeMillis()-tiempoIni) + "ms");
+    }
+}
+
+class Persona
+{ 
+    private String nombre;
+   
+    public Persona(String nombre) 
+    {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() 
+    {
+        return nombre;
+    }    
+    
+    @Override
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Nombre: ")
+          .append(nombre);
+        return sb.toString();
+    }
+    
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(obj instanceof Persona p) 
+            return nombre.equals(p.getNombre());
+        else
+            return false;
+    }
+}
