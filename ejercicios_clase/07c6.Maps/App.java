70433ec12c83c253274e168b41e2514c48115cf7
David Contreras <davidcb@comillas.edu>
2023-03-30 16:04:11 +0200

    Actualización de tratamiento de Maps con la estructura for each

--- ejercicios_clase/07c6.Maps/App.java
+++ ejercicios_clase/07c6.Maps/App.java
@@ -21,12 +21,12 @@ public class App
 {
     public static void main(String[] args)
     {
-        Map personas;
+        Map<Persona, Coche> personas;
 
         if(true)
-            personas = new HashMap();
+            personas = new HashMap<>();
         else
-            personas = new TreeMap();
+            personas = new TreeMap<>();
 
         personas.put(new Persona("11111A", "Ana", 22), new Coche("VW", "Golf"));
         personas.put(new Persona("22222A", "Cristina", 33), new Coche("Audi", "A3"));
@@ -46,53 +46,36 @@ public class App
         
         System.out.println("\nListado de Personas");
         System.out.println("===============");
-        Set personasSet = personas.keySet();
-        Iterator it = personasSet.iterator();
-        while(it.hasNext())
+        Set<Persona> personasSet = personas.keySet();
+        Iterator<Persona> itPersonas = personasSet.iterator();
+        while(itPersonas.hasNext())
         {
-            Object o = it.next();
-            System.out.println(o);
-            Persona p = (Persona) o;
+            Persona p = itPersonas.next();
             System.out.println(p.getNombre());
         }
+
+        //Trabajando con estructura for each
+        for(Persona p : personas.keySet())
+            System.out.println(p.getNombre());
             
         System.out.println("\nListado de coches");
         System.out.println("===============");
-        Collection personasValues = personas.values();
-        it = personasValues.iterator();
-        while(it.hasNext())
+        Collection<Coche> coches = personas.values();
+        Iterator<Coche> itCoches = coches.iterator();
+        while(itCoches.hasNext())
         {
-            Object o = it.next();
-            System.out.println(o);
-            Coche c = (Coche) o;
+            Coche c = itCoches.next();
             System.out.println(c.getMarca());
         }
 
-        
+        //Trabajando con estructura for each
+        for(Coche c : personas.values())
+            System.out.println(c.getMarca());
+
+
         System.out.println("\nListado de los dos objetos (clave+valor) del Map");
         System.out.println("===============");
-        Set personasEntrySet = personas.entrySet();
-        it = personasEntrySet.iterator();
-        while(it.hasNext())
-        {
-            Object o = it.next();
-            Map.Entry entry = (Map.Entry) o;
-            System.out.println("- La clave:\n" + entry.getKey() + "\n- El valor:\n" + entry.getValue());     
-        }
-        
-
-        System.out.println("\nListado con la necesidad de realizar DOWNCASTING del Map");
-        System.out.println("===============");
-        it = personasEntrySet.iterator();
-        while(it.hasNext())
-        {
-            Object o = it.next();
-            Map.Entry entry = (Map.Entry) o;
-            System.out.println("- La clave: " + entry.getKey() + "\n- El valor: " + entry.getValue());     
-
-            Persona p = (Persona) entry.getKey();
-            Coche c = (Coche) entry.getValue();            
-            System.out.println(p.getNombre() + " tiene un " + c.getMarca());     
-        }
+        for(Map.Entry<Persona, Coche> personasEntrySet : personas.entrySet())
+           System.out.println("- La clave: " + personasEntrySet.getKey() + " - El valor: " + personasEntrySet.getValue());     
     }    
 }
